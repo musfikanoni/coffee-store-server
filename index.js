d85c01631114cf81b54a6bd38e40d2a72d29d3be
musfikanoni <musfikanoni@gmail.com>
2024-12-06 10:48:07 +0600

    delete update

--- index.js
+++ index.js
@@ -40,10 +40,39 @@ async function run() {
     })
 
 
+    app.get('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+
+
     app.post('/coffee', async(req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    app.put('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updateCoffee = req.body;
+      const coffee = {
+        $set: {
+          name: updateCoffee.name,
+          quantity: updateCoffee.quantity,
+          supplier: updateCoffee.supplier,
+          taste: updateCoffee.taste, 
+          category: updateCoffee.category,
+          details: updateCoffee.details,
+          photo: updateCoffee.photo
+        }
+      }
+      
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
       res.send(result);
     })
 
